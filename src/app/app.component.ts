24397c94102bb3f990ef1830f113a82f1dcaf1e5
Andrés Felipe Largo Rodríguez <alargo@dbsolution.com.co>
2019-08-20 14:02:45 -0500

    Calculadora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calc';
+  operation = '';
+  state = 'initial';
+  add(numb: string) {
+    (this.state === 'calculated') ? this.clean(numb) : this.operation = this.operation + (numb);
+  }
+
+  calc() {
+    try {
+      if (this.operation.search('/0') > 0){
+        throw new Error('No se puede dividir en cero');
+      }
+      // tslint:disable-next-line: no-eval
+      this.operation = eval(this.operation);
+    } catch (e) {
+      alert('Escriba bien los números.');
+    }
+    this.state = 'calculated';
+  }
+
+  clean(numb: string){
+    this.state = 'initial';
+    this.operation = '';
+    if(numb){
+      this.add(numb);
+    }
+  }
 }
